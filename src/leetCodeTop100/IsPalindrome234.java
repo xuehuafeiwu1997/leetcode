0697bf2f5add094ffb27cdbaf8dab04b53241d3f
xuehuafeiwu1997 <2500207996@qq.com>
2023-03-19 19:34:22 +0800

    234.判断是否是回文链表，将其复制为数组，然后在进行转换

---
+++ src/leetCodeTop100/IsPalindrome234.java
@@ -0,0 +1,32 @@
+package leetCodeTop100;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 234.回文链表
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+ *
+ * @author xmy
+ * @date 2023/3/19 7:13 下午
+ */
+public class IsPalindrome234 {
+
+    public boolean isPalindrome(ListNode head) {
+        //转换为数组的思想
+        List<Integer> arr = new ArrayList<>();
+        ListNode currentNode = head;
+        //将其复制为数组
+        while (currentNode != null) {
+            arr.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+        for (int i = 0; i <= arr.size() / 2 ; i++) {
+            if (arr.get(i) != arr.get(arr.size() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
